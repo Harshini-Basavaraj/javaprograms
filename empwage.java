55ac2d60497ae34c5b5697042123459ae14273e9
Harshini-Basavaraj <harshinihb83@gmail.com>
2021-09-02 22:11:30 +0530

    Added:My first java program

---
+++ empwage.java
@@ -0,0 +1,11 @@
+ public class empwage {
+
+	public static void main(String[] args) {
+       	 int IS_FULL_TIME = 1;
+         double empcheck = Math.floor(Math.random() * 10) % 2;
+         if (empcheck == IS_FULL_TIME)
+            System.out.println("Emp is present");
+         else
+     	    System.out.println("Emp is absent");
+	}
+}
